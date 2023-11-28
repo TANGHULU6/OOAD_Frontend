627d65d37d926e8e8617e8715d19dee6a8270303
TANGHULU6 <12110502@mail.sustech.edu.cn>
2023-11-29 02:41:00 +0800

    api fixed

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -92,3 +92,114 @@ export async function removeRule(options?: { [key: string]: any }) {
     ...(options || {}),
   });
 }
+// 获取课程列表
+export async function getCourseList(options?: { [key: string]: any }) {
+  return request<API.CourseList>('/api/course/list', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+// 更新课程信息
+export async function updateCourse(params: { id: number; courseName: string; teacherId: number; taIdList: number[] }, options?: { [key: string]: any }) {
+  return request<boolean>('/api/course/update', {
+    method: 'POST',
+    data: {
+      ...params
+    },
+    ...(options || {}),
+  });
+}
+// 删除课程
+export async function removeCourse(params: { id: number }, options?: { [key: string]: any }) {
+  return request<boolean>('/api/course/delete', {
+    method: 'POST',
+    data: {
+      ...params
+    },
+    ...(options || {}),
+  });
+}
+// 创建新课程
+export async function addCourse(params: { courseName: string; teacherId: number; taIdList: number[] }, options?: { [key: string]: any }) {
+  return request<{ courseId: bigint }>('/api/course/insert', {
+    method: 'POST',
+    data: {
+      ...params
+    },
+    ...(options || {}),
+  });
+}
+// 获取课程详情
+export async function getCourseDetail(courseId: number, options?: { [key: string]: any }) {
+  return request<API.CourseDetail>('/api/course', {
+    method: 'GET',
+    params: { courseId }, // 将 courseId 作为查询参数传递
+    ...(options || {}),
+  });
+}
+
+// 任命教师助理
+export async function appointTA(courseId: number, taId: number, options?: { [key: string]: any }) {
+  return request<API.TAResponse>(`/api/course/add/ta`, {
+    method: 'POST',
+    data: { courseId, taId },
+    ...(options || {}),
+  });
+}
+
+// 免职教师助理
+export async function dismissTA(courseId: number, taId: number, options?: { [key: string]: any }) {
+  return request<API.TAResponse>(`/api/course/remove/ta`, {
+    method: 'POST',
+    data: { courseId, taId },
+    ...(options || {}),
+  });
+}
+
+// 添加学生
+export async function addStudent(courseId: number, studentId: number, options?: { [key: string]: any }) {
+  return request<API.StudentResponse>(`/api/course/add/student`, {
+    method: 'POST',
+    data: { courseId, studentId },
+    ...(options || {}),
+  });
+}
+
+// 移除学生
+export async function removeStudent(courseId: number, studentId: number, options?: { [key: string]: any }) {
+  return request<API.StudentResponse>(`/api/course/remove/student`, {
+    method: 'POST',
+    data: { courseId, studentId },
+    ...(options || {}),
+  });
+}
+
+// 获取所有教师
+export async function getAllTeachers(options?: { [key: string]: any }) {
+  return request<API.TeacherList>(`/api/user/listAllTeacherName`, {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
+// 获取所有学生
+export async function getAllStudents(options?: { [key: string]: any }) {
+  return request<API.StudentList>(`/api/user/listAllStudentName`, {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+export async function getCourseNotifications(courseId: number, options?: { [key: string]: any }) {
+  return request<API.NoticeIconList>(`/api/course/notification/list`, {
+    method: 'GET',
+    params: { courseId }, // Passing courseId as a query parameter
+    ...(options || {}),
+  });
+}
+export async function deleteCourseNotification(notificationId: number, options?: { [key: string]: any }) {
+  return request<boolean>('/api/course/notification/delete', {
+    method: 'POST',
+    data: { notificationId }, // Sending notificationId in the request body
+    ...(options || {}),
+  });
+}
--- src/services/ant-design-pro/typings.d.ts
+++ src/services/ant-design-pro/typings.d.ts
@@ -113,4 +113,60 @@ declare namespace API {
     description?: string;
     type?: NoticeIconItemType;
   };
+
+
+  type ApiResponse = {
+    code?: number;
+    type?: string;
+    message?: string;
+  };
+  type Course = {
+    id?: number;
+    name: string;
+    teacherId: number;
+    taIdList?: number[];
+    studentCount?: number;
+    creationTime?: string;
+  };
+
+  type Teacher = {
+    id: number;
+    name: string;
+    // ... 其他教师相关属性 ...
+  };
+
+  type Student = {
+    id: number;
+    name: string;
+    // ... 其他学生相关属性 ...
+  };
+
+  // 其他必要的类型声明，如课程列表、教师列表等
+  type CourseList = Course[];
+  type TeacherList = Teacher[];
+  type StudentList = Student[];
+  // 教师助理响应类型
+  type TAResponse = {
+    success: boolean;
+    message?: string;
+    // 可以添加其他响应字段
+  };
+
+  // 学生响应类型
+  type StudentResponse = {
+    success: boolean;
+    message?: string;
+    // 可以添加其他响应字段
+  };
+
+  // 课程详细信息类型
+  type CourseDetail = {
+    id: number;
+    name: string;
+    teacher: Teacher; // 假设教师信息是完整的对象
+    teachingAssistants: Teacher[]; // 假设助教信息是教师对象的数组
+    students: Student[]; // 假设学生信息是学生对象的数组
+    creationTime: string;
+    // 可以添加其他课程相关字段
+  };
 }
